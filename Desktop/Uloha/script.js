c1970d13dc5d8d26dcabb3417ce4a48ca6cfa3ca
RobAnt13 <robert.antony050607@gmail.com>
2022-11-17 19:32:33 +0100

    fibonacci vo funkcii

---
+++ Desktop/Uloha/script.js
@@ -0,0 +1,11 @@
+// Fibonacci 
+
+let fibSeq = function(){
+    let sum = 1
+    for (let i = 0; i < 100; i = i + sum) {
+        sum = sum + i;
+    }      
+    return sum;
+}
+console.log(fibSeq());
+
